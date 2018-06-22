fc18c762b1f6df5c3ff8061591c76a16aefc720f
TheDeadLizard <thelizardbusiness@gmail.com>
2018-06-22 15:37:06 +0530

    Silent Curl, Sleep before initial

--- installer.sh
+++ installer.sh
@@ -2,14 +2,17 @@
 #might need root permission to copy into /usr/bin
 echo "Installing TorNotify"
 echo "Scripted by deadlizard@tutanota.com,https://twitter.com/TheDeadLizard1"
+echo "Cron set to run every five minutes. Please change as you wish"
 sudo cp ./tornotify /usr/bin/tornotify #copy main file into bin
 sudo chmod 777 /usr/bin/tornotify
 #write out current crontab
 crontab -l > /tmp/mycron
 #echo new cron into cron file
-echo "*/5 * * * * /usr/bin/tornotify" >> /tmp/mycron
+echo "*/5 * * * * /usr/bin/tornotify" >> /tmp/mycron #every 5 minutes
 #install new cron file
 crontab /tmp/mycron
 rm /tmp/mycron
+sleep 3
+
 tornotify
 #enter new entry into crontab to show the stuff every 3 minutes.
