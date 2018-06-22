48873aba0aef7e0d546be7fdc57cdc26be5656ec
TheDeadLizard <thelizardbusiness@gmail.com>
2018-06-22 15:33:08 +0530

    Added Credits

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #might need root permission to copy into /usr/bin
 echo "Installing TorNotify"
-echo "deadlizard@tutanota.com,https://twitter.com/TheDeadLizard1"
+echo "Scripted by deadlizard@tutanota.com,https://twitter.com/TheDeadLizard1"
 sudo cp ./tornotify /usr/bin/tornotify #copy main file into bin
 sudo chmod 777 /usr/bin/tornotify
 #write out current crontab
