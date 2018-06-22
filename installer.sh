3f2d23f5a9bdb1eb8f7c4bd16fae53a0aeee7eff
TheDeadLizard <thelizardbusiness@gmail.com>
2018-06-22 15:31:17 +0530

    Installer first run

--- installer.sh
+++ installer.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 #might need root permission to copy into /usr/bin
-#copy main file into bin
+echo "Installing TorNotify"
+echo "deadlizard@tutanota.com,https://twitter.com/TheDeadLizard1"
+sudo cp ./tornotify /usr/bin/tornotify #copy main file into bin
+sudo chmod 777 /usr/bin/tornotify
+#write out current crontab
+crontab -l > /tmp/mycron
+#echo new cron into cron file
+echo "*/5 * * * * /usr/bin/tornotify" >> /tmp/mycron
+#install new cron file
+crontab /tmp/mycron
+rm /tmp/mycron
+tornotify
 #enter new entry into crontab to show the stuff every 3 minutes.
