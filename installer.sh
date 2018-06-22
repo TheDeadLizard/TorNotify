e506a8b8fb615a7ec24acfd01e53d82e48ce3e3a
TheDeadLizard <thelizardbusiness@gmail.com>
2018-06-22 17:09:19 +0530

    Final Changes Added for today

--- installer.sh
+++ installer.sh
@@ -3,8 +3,10 @@
 echo "Installing TorNotify"
 echo "Scripted by deadlizard@tutanota.com,https://twitter.com/TheDeadLizard1"
 echo "Cron set to run every five minutes. Please change as you wish"
+echo "No crontab for root"
 sudo cp ./tornotify /usr/bin/tornotify #copy main file into bin
 sudo chmod 777 /usr/bin/tornotify
+sudo apt-get --assume-yes install jq #install jq from ubuntu repo
 #write out current crontab
 crontab -l > /tmp/mycron
 #echo new cron into cron file
